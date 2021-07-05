d6a599f1ed354ce3b39d04553baea38cb99ac58a
f102ngtyuj <f102.nagata.yuuji@gmail.com>
2021-07-05 17:12:51 +0900

    不具合の修正（１回目）

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :encrypted_password, presence: true, length: { minimum: 6 }, confirmation: true
+  # validates :email, presence: true, uniqueness: true
+  # validates :encrypted_password, presence: true, length: { minimum: 6 }, confirmation: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
