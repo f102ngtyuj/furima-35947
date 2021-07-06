4847fec604d116cbb9e9b87de035fe8da399d10e
f102ngtyuj <f102.nagata.yuuji@gmail.com>
2021-07-06 20:42:14 +0900

    ActiveHash修正（１回目）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :burden
+  belongs_to_active_hash :area
+  belongs_to_active_hash :shipping_date
   has_one_attached :image
 
   with_options presence: true do
