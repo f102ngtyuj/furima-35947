7baa7dd042c108e43fc5bee2cd67b6a5d129fe6b
f102ngtyuj <f102.nagata.yuuji@gmail.com>
2021-07-06 17:53:21 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,18 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  with_options presence: true do
+    validates :user
+    validates :product_name
+    validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+    validates :description
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :burden_id
+    validates :area_id
+    validates :shipping_date_id
+  end
 end
