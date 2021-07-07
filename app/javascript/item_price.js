898983f2f3bb82191d0165c7c99df31dc19d9f62
f102ngtyuj <f102.nagata.yuuji@gmail.com>
2021-07-07 19:49:18 +0900

    必要な要素の取得（Javascript）

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,15 @@
 window.addEventListener('load', () => {
   console.log("OK");
+  const priceInput = document.getElementById("item-price");
+  console.log(priceInput);
+});
+
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("add-tax-price");
+  console.log(priceInput);
+});
+
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("profit");
+  console.log(priceInput);
 });
