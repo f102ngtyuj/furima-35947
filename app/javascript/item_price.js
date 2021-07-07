5568ce747e1ad6b6da05046646bdc1e0e4f04dea
f102ngtyuj <f102.nagata.yuuji@gmail.com>
2021-07-08 00:16:39 +0900

    販売手数料、利益計算結果の表示（及び記述内容のまとめ作業）

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,17 +2,9 @@ window.addEventListener('load', () => {
   console.log("OK");
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
-    console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
+    const addProfitDom = document.getElementById("profit");
+    addProfitDom.innerHTML = Math.floor(priceInput.value - Math.round(priceInput.value * 0.1 ))
   })
-});
-
-window.addEventListener('load', () => {
-  const priceInput = document.getElementById("add-tax-price");
-  console.log(priceInput);
-});
-
-window.addEventListener('load', () => {
-  const priceInput = document.getElementById("profit");
-  console.log(priceInput);
 });
