08b67fb8547c4210358b8efa9e222d2dfb9f171e
f102ngtyuj <f102.nagata.yuuji@gmail.com>
2021-07-13 21:26:42 +0900

    コードレビュー後修正（２回目）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_action, except: [:index, :new, :create]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy] 
-  before_action :sold_out,only: [:edit, :update]
+  before_action :sold_out,only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order("created_at DESC")
