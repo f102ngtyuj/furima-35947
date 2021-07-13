4cff3e98576b127a23eebfec95e7e4c1c0dfb5a0
f102ngtyuj <f102.nagata.yuuji@gmail.com>
2021-07-13 21:44:23 +0900

    コードレビュー後修正（３回目）

--- db/schema.rb
+++ db/schema.rb
@@ -88,7 +88,7 @@ ActiveRecord::Schema.define(version: 2021_07_11_002214) do
     t.integer "area_id", null: false
     t.string "municipality", null: false
     t.string "address", null: false
-    t.string "building_name", null: false
+    t.string "building_name"
     t.string "phone_number", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
