944b2f2af9fbdc04cca1df137e1c0661efb96e7c
f102ngtyuj <f102.nagata.yuuji@gmail.com>
2021-07-13 19:00:19 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_action, except: [:index, :new, :create]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy] 
+  before_action :sold_out,only: [:edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -52,5 +53,9 @@ class ItemsController < ApplicationController
 
   def contributor_confirmation
     redirect_to root_path unless current_user.id == @item.user.id
+  end
+
+  def sold_out
+    redirect_to root_path if @item.order.present?
   end
 end
