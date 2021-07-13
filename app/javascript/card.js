aae0eb70da7d1fe9566c9b7efdfc88cc896364fd
f102ngtyuj <f102.nagata.yuuji@gmail.com>
2021-07-13 12:18:15 +0900

    ブラウザ上のフォーム情報取得のための記述

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,18 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_ca388c36555bf0fae016810b");
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("購入時にイベント発火")
+    
+    const formResult = document.getElementById("charge-form");
+      const formData = new FormData(formResult);
+
+      const card = {
+        number: formData.get("number"),
+        cvc: formData.get("cvc"),
+        exp_month: formData.get("exp_month"),
+        exp_year: `20${formData.get("exp_year")}`,
+      };
   });
 };
 
