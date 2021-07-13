6937955c3d99ea2f16c7aae6706a87ac52d8a031
f102ngtyuj <f102.nagata.yuuji@gmail.com>
2021-07-13 14:36:23 +0900

    カード情報のトークン化

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -5,14 +5,23 @@ const pay = () => {
     e.preventDefault();
     
     const formResult = document.getElementById("charge-form");
-      const formData = new FormData(formResult);
+    const formData = new FormData(formResult);
 
-      const card = {
-        number: formData.get("number"),
-        cvc: formData.get("cvc"),
-        exp_month: formData.get("exp_month"),
-        exp_year: `20${formData.get("exp_year")}`,
-      };
+    const card = {
+      number: formData.get("purchase_shipping[number]"),
+      cvc: formData.get("purchase_shipping[cvc]"),
+      exp_month: formData.get("purchase_shipping[exp_month]"),
+      exp_year: `20${formData.get("purchase_shipping[exp_year]")}`,
+    };
+    console.log(card)
+
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
   });
 };
 
